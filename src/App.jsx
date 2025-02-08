893729f0de4fc99ce64540e9f47494a4ab0f432b
Tejus <tejusraghavendra09@gmail.com>
2025-02-09 03:08:34 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-import { Hero } from "./components/Hero";
+import Hero from "./components/Hero";
+import BrowseJobs from "./components/BrowseJobs";
+import Navbar from "./components/Navbar";
+import DevelopersAndEmployers from "./components/DevelopersAndEmployers";
 
 const App = () => {
-  return <Hero />;
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <DevelopersAndEmployers />
+      <BrowseJobs />
+    </>
+  );
 };
 
 export default App;
