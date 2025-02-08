2a3c58da1f5d2a52920fd8d9da55df645a3e1b0c
Tejus <tejusraghavendra09@gmail.com>
2025-02-09 01:28:37 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,7 @@ import React from "react";
 import { Hero } from "./components/Hero";
 
 const App = () => {
-  return (
-    <>
-      <h1>sodvnbod</h1>
-      {/* <section class="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
-      <Hero /> */}
-    </>
-  );
+  return <Hero />;
 };
 
 export default App;
